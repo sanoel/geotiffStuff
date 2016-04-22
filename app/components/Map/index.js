2088d4c28477d9f14c27bc7dd18e811e4e8bd55e
Sam Noel <noel.samuel.a@gmail.com>
2016-04-22 15:21:43 -0400

    test image on canvas

--- app/components/Map/index.js
+++ app/components/Map/index.js
@@ -3,6 +3,7 @@ import { Decorator as Cerebral, Link } from 'cerebral-view-react';
 import { Polygon, Marker, Map, TileLayer, ImageOverlay, latLng, latLngBounds} from 'react-leaflet';
 import RasterLayer from '../RasterLayer';
 import styles from './map.css';
+console.log(RasterLayer);
 
 @Cerebral((props) => {
   return {
--- app/components/RasterLayer/index.js
+++ app/components/RasterLayer/index.js
@@ -1,6 +1,7 @@
 import { Decorator as Cerebral } from 'cerebral-view-react';
+import React from 'react';
 import styles from './style.css';
-
+import { CanvasTileLayer } from 'react-leaflet';
 
 @Cerebral((props) => {
   return {
@@ -8,12 +9,12 @@ import styles from './style.css';
 })
 
 class _RasterLayer extends React.Component {
-  
 
   componentWillMount() {
     this.container = document.getElementById('hidden-stuff');
     this.canvas = document.createElement('CANVAS');
-    this.canvas.style.visibility = 'hidden';
+//    this.canvas.style.visibility = 'hidden';
+    this.canvas.style.visibility = 'visible';
     this.container.appendChild(this.canvas);
   }
 
@@ -23,13 +24,29 @@ class _RasterLayer extends React.Component {
 
   render() {
     // create an image in the hidden canvas from the props
+    var ctx = this.canvas.getContext('2d');
+    var width = raster.data.length;
+    var height = raster.data[0].length;
+    var img = ctx.createImageData(width, height);
+    var data = img.data;
+    for (var i = 0; i < width*height*4; i++) {
+      if (i == 0) {
+        data[i]     = 255;     // red
+        data[i + 1] = 0; // green
+        data[i + 2] = 0; // blue
+      } else {
+        data[i]     = 0;     // red
+        data[i + 1] = 255; // green
+        data[i + 2] = 0; // blue
+      }
+    } 
+    ctx.putImageData(img, 0, 0);
     return (
       <div>
         <RasterTileInternalLayer canvas={this.canvas} />
       </div>
     );
   }
-
 }
 
 class RasterTileInternalLayer extends CanvasTileLayer {
@@ -38,7 +55,8 @@ class RasterTileInternalLayer extends CanvasTileLayer {
     this.leafletElement.drawTile = this.drawTile.bind(this);
   }
   drawTile(canvas, tilePoint, zoom) {
+    
   }
 }
 
-export default _RasterLayer;
+export {_RasterLayer, RasterTileInternalLayer};
