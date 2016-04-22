92f9c94e636a1cb41c3a839af1c45f17804e6e48
Sam Noel <noel.samuel.a@gmail.com>
2016-04-22 14:09:06 -0400

    rasterlayer

---
+++ app/components/RasterLayer/index.js
@@ -0,0 +1,13 @@
+import React, { Proptypes } from 'react';
+
+class _RasterLayer extends React.Component {
+  render() {
+
+    return (
+      <div id='raster-layer' >
+        <canvas />      
+
+      </div>
+    );
+  }
+}
