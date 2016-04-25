af80f660a2213a947f16ffb0002296b5c2e8b3e3
Sam Noel <noel.samuel.a@gmail.com>
2016-04-25 14:35:59 -0400

    correctly positioning image on map

--- app/components/RasterLayer/index.js
+++ app/components/RasterLayer/index.js
@@ -27,49 +27,57 @@ export default class RasterLayer extends CanvasTileLayer {
   }
 
   drawTile(canvas, tilePoint, zoom) {
-    let ctx = canvas.getContext('2d');
-    var southWest = this.props.map.unproject(new L.Point(tilePoint.x*256, (tilePoint.y*256)+256), zoom);
-    var northEast = this.props.map.unproject(new L.Point((tilePoint.x*256)+256, tilePoint.y*256), zoom);
     var raster = this.props.raster;
-    var tifImgData = this.getImgData(southWest, northEast);
-    console.log(tifImgData);
-    if (tifImgData) {
-      var topleftpt  = this.props.map.project(L.latLng(raster.geotransform.topleft.lat, raster.geotransform.topleft.lon), zoom);
-      console.log(topleftpt);
-      ctx.putImageData(tifImgData, topleftpt.x%256, topleftpt.y%256);
-    }
+    var ctx = canvas.getContext('2d');
+    var tileSwPt = new L.Point(tilePoint.x*256, (tilePoint.y*256)+256);
+    var tileNePt = new L.Point((tilePoint.x*256)+256, tilePoint.y*256);
 
-    //draw grid lines
-    var padding = 0;
-    var totalExtent = 4096 * (1 + padding * 2);
-    var height = canvas.height;
-    var ratio = height / totalExtent;
-    var pad = 4096 * padding * ratio;
-    var height = canvas.height;
-    var halfHeight = height / 2;
-    ctx.strokeStyle = 'lightgreen';
-    ctx.strokeRect(pad, pad, height - 2 * pad, height - 2 * pad);
-    ctx.beginPath();
-    ctx.moveTo(pad, halfHeight);
-    ctx.lineTo(height - pad, halfHeight);
-    ctx.moveTo(halfHeight, pad);
-    ctx.lineTo(halfHeight, height - pad);
-    ctx.stroke();
-  }
+    var tileSw = this.props.map.unproject(tileSwPt, zoom);
+    var tileNe = this.props.map.unproject(tileNePt, zoom);
+    var tileBounds = L.latLngBounds(tileSw, tileNe);
 
-  getImgData(southWest, northEast) {
-    var raster = this.props.raster;
-    var tileBounds = L.latLngBounds(southWest, northEast);
     var topLeft = raster.geotransform.topleft;
-    var gtifSouthWest = L.latLng(topLeft.lat + raster.geotransform.cellspacing.lat*raster.data.length, topLeft.lon);
-    var gtifNorthEast = L.latLng(topLeft.lat, topLeft.lon + raster.geotransform.cellspacing.lon*raster.data[0].length);
-    var gtifBounds = L.latLngBounds([gtifSouthWest, gtifNorthEast]);
-    if (tileBounds.intersects(gtifBounds)) {
-      console.log('great success');
-      return this.canvas.getContext('2d').getImageData(0, 0, this.canvas.width, this.canvas.height);
+    var dataSw = L.latLng(topLeft.lat + raster.geotransform.cellspacing.lat*raster.data.length, topLeft.lon);
+    var dataNe = L.latLng(topLeft.lat, topLeft.lon + raster.geotransform.cellspacing.lon*raster.data[0].length);
+    var dataBounds = L.latLngBounds([dataSw, dataNe]);
+ 
+    var dataSwPt = this.props.map.project(dataSw);
+    var dataNePt = this.props.map.project(dataNe);
+    console.log(dataSw);
+    console.log(dataNe);
+
+    // Determine bounds of image data in the hidden image canvas space 
+    if (tileBounds.intersects(dataBounds)) {
+      console.log('tile contains some data');
+
+      var tileLeft = dataSwPt.x - tileSwPt.x;
+//      var tileLeft =  dataSwPt.x%256;
+      var tileTop =  dataNePt.y - tileNePt.y;
+      //var tileTop =  dataNePt.y%256;
+      var tileHeight = (dataSwPt.y - dataNePt.y);
+      var tileWidth = (dataNePt.x - dataSwPt.x);
+      console.log(tileLeft, tileTop, tileHeight, tileWidth);
+      //get image data from hidden image
+      //var imgData = this.canvas.getContext('2d').getImageData(0, 0, this.canvas.width, this.canvas.height);
+
+      // Put it on the tile
+      ctx.drawImage(this.canvas, tileLeft, tileTop, tileWidth, tileHeight);
+
+      // draw the tile grids
+      var padding = 0;
+      var totalExtent = 4096 * (1 + padding * 2);
+      var height = canvas.height;
+      var ratio = height / totalExtent;
+      var pad = 4096 * padding * ratio;
+      var height = canvas.height;
+      ctx.strokeStyle = 'lightgreen';
+      ctx.strokeRect(pad, pad, height - 2 * pad, height - 2 * pad);
+      ctx.beginPath();
+      ctx.stroke();
     }
   }
 
+    
   colorForValue(val) {
     var raster = this.props.raster;
     var levels = raster.legend.levels;
@@ -90,20 +98,19 @@ export default class RasterLayer extends CanvasTileLayer {
     var raster = this.props.raster;
     // create an image in the hidden canvas from the props
     var ctx = this.canvas.getContext('2d');
-    var width = raster.data.length;
-    var height = raster.data[0].length;
+    var width = raster.data[0].length;
+    var height = raster.data.length;
     this.canvas.width = width;
     this.canvas.height = height;
     var img = ctx.createImageData(width, height);
     var data = img.data;
     for (var j = 0; j < height; j++) {
       for (var i = 0; i < width; i++) {
-         let color = this.colorForValue(raster.data[i][j]);
-         data[(i*width+j)*4]   = color.r; // red
-         data[(i*width+j)*4+1] = color.g; // green
-         data[(i*width+j)*4+2] = color.b; // blue
-         data[(i*width+j)*4+3] = 255; // blue
-//         data[j*width+i+3] = (typeof color.a !== 'undefined') ? color.a : 0; // alpha
+         let color = this.colorForValue(raster.data[j][i]);
+         data[((j*width+i)*4)]   = color.r; // red
+         data[((j*width+i)*4)+1] = color.g; // green
+         data[((j*width+i)*4)+2] = color.b; // blue
+         data[((j*width+i)*4)+3] = 255;     // alpha
       }
     } 
     ctx.putImageData(img, 0, 0);
