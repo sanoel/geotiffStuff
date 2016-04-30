36256ffd68e75f38a5bb6a4f2a81569cbfeae2e0
Aaron Ault <ault@purdue.edu>
2016-04-30 18:32:43 -0400

    Trying port 3000 for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/dist'));
 
 var port = 3000;
 if (process.env.NODE_ENV === 'production') {
-  port = 80;
+//  port = 80;
 }
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
