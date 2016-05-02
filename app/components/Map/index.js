6b758db2c0eb9148796194e1bef0569e5cc91d1b
Sam Noel <noel.samuel.a@gmail.com>
2016-05-02 08:17:43 -0400

    fixing leaflet css for heroku

--- app/components/Map/index.js
+++ app/components/Map/index.js
@@ -64,6 +64,7 @@ class _Map extends React.Component {
           zoom={11}
           onLocationfound={(e) => {signals.locationFound({latlng:e.latlng, accuracy:e.accuracy})}}
           locationError={console.log("Location Error!")}>
+          <GeoJSON data={lots} />
           <TileLayer
             url='http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
             attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
