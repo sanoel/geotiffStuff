5bbadac8563d19ec75344d977fdc176ceda062b6
Aaron Ault <ault@purdue.edu>
2016-05-06 16:20:42 -0400

    added logins

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var passport = require('passport');
+var DigestStrategy = require('passport-http').DigestStrategy;
 
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+passport.use(new DigestStrategy({ qop: 'auth' },
+  function(username, done) {
+    if (username !== 'alberto') return done(null, false);
+    return done(null, { username: 'alberto' }, 'samisamazing');
+  },
+  function(params, done) {
+    // validate nonces as necessary
+    done(null, true)
+  }
+));
+
+app.use('/', [ 
+  passport.authenticate('digest', { session: false }), 
+  express.static(__dirname + '/dist')]
+);
 
 var port = 3000;
 if (process.env.NODE_ENV === 'production') {
