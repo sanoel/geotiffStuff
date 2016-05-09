c5d89d046f5ee475b6a5c7179296514128047669
Sam Noel <noel.samuel.a@gmail.com>
2016-05-09 14:00:58 -0400

    fixed extraneous dependencies, all maps working; no tile caching yet

--- app/components/Map/index.js
+++ app/components/Map/index.js
@@ -111,7 +111,6 @@ class _Map extends React.Component {
           <TileLayer 
             url='http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
             attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-            useCache={true}
           />
   
           <RasterLayer 
